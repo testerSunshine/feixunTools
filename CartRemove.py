56b1f99236e56cad0592533beca6e256ceaa4d0a
wenxianping <931128603@qq.com>
2018-11-18 11:01:47 +0800

    id

--- CartRemove.py
+++ CartRemove.py
@@ -14,7 +14,7 @@ def cartRemove(session, goods_id):
     cartRemoveRps = session.httpClint.send(urls.get("cartRemove" ""), data)
     while not cartRemoveRps.get("error", "") == "购物车为空":
         if cartRemoveRps.get("result", "") == "success":
-            goods_id = cartRemoveRps["data"]["objects"]["goods"]
+            goods_id = cartRemoveRps["data"]["objects"]["goods"]["obj_ident"]
             U.Logging.info("当前删除购物车商品id为: {}".format(goods_id))
             cartRemove(session, goods_id)
         else:
